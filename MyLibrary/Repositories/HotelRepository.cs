2c2132548e1b0bb74646ff56e62e26169f3095d5
huylinhit <huylinhit@gmail.com>
2023-04-24 12:27:45 +0700

    Repositories Update

---
+++ MyLibrary/Repositories/HotelRepository.cs
@@ -0,0 +1,23 @@
+﻿using MyLibrary.DAOs;
+using MyLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Repositories
+{
+    public class HotelRepository : IHotelInterface
+    {
+        public void AddHotel(Hotel hotel) => HotelDAO.Instance.AddHotel(hotel);
+
+        public void DeleteHotel(int id) => HotelDAO.Instance.RemoveHotelByID(id);
+
+        public Hotel GetHotelById(int id) => HotelDAO.Instance.GetHotelByID(id);
+
+        public IEnumerable<Hotel> GetHotels() => HotelDAO.Instance.GetHotels();
+
+        public void UpdateHotel(Hotel hotel) => HotelDAO.Instance.UpdateHotel(hotel);
+    }
+}
